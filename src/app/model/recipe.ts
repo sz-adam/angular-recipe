ffb4f8ffa4a9efcf16cc5bf8e17bed50b7ac500f
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-20 22:49:17 +0100

    delete id

--- src/app/model/recipe.ts
+++ src/app/model/recipe.ts
@@ -1,18 +1,16 @@
 import { Ingredient } from "./ingredient";
 
-export class Recipe {
-    id:number = 0;
+export class Recipe {  
     name: string ='';
     description: string ='';
     imagePath: string ='';
     ingredients :Ingredient[] = []
   
 
-    constructor(id: number, name: string, desc: string, imagePath: string, ingredients:Ingredient[]) {
+    constructor( name: string, desc: string, imagePath: string, ingredients:Ingredient[]) {
         this.name = name;
         this.description = desc;
-        this.imagePath =imagePath; 
-        this.id = Math.floor(Math.random() * 1000)
+        this.imagePath =imagePath;         
         this.ingredients = ingredients
     }
 }
