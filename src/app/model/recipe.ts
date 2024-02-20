4a74636ab7be13fa6750bf39aff151ea1d9daeb0
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-20 18:33:06 +0100

    create recipe model

--- src/app/model/recipe.ts
+++ src/app/model/recipe.ts
@@ -1,2 +1,14 @@
 export class Recipe {
+    id:number = 0;
+    name: string ='';
+    description: string ='';
+    imagePath: string ='';
+  
+
+    constructor(id: number, name: string, desc: string, imagePath: string, ) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath =imagePath; 
+        this.id = id;     
+    }
 }
