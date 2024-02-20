8cd2f5a7be65f2ccdd17262d9e9648771f0e17b7
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-20 19:01:09 +0100

    add navigation and style

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RecipeComponent } from './pages/recipe/recipe.component';
+import { ShoppingComponent } from './pages/shopping/shopping.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+
+  {
+    path: 'recipes',
+    component: RecipeComponent,
+  },
+  { path: 'shopping', component: ShoppingComponent },
+];
