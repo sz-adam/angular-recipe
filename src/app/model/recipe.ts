9b43e2708759b32e1e0ea09213e28b1d91a5b9be
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-20 19:54:20 +0100

    recipe model supplement ingredient

--- src/app/model/recipe.ts
+++ src/app/model/recipe.ts
@@ -1,14 +1,18 @@
+import { Ingredient } from "./ingredient";
+
 export class Recipe {
     id:number = 0;
     name: string ='';
     description: string ='';
     imagePath: string ='';
+    ingredients :Ingredient[] = []
   
 
-    constructor(id: number, name: string, desc: string, imagePath: string, ) {
+    constructor(id: number, name: string, desc: string, imagePath: string, ingredients:Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath =imagePath; 
-        this.id = id;     
+        this.id = Math.floor(Math.random() * 1000)
+        this.ingredients = ingredients
     }
 }
