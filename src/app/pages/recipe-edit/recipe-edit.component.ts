87a90d252f16cda8ecbfccdcbd9da0091651048a
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-21 20:36:36 +0100

    add ingredients edit component

--- src/app/pages/recipe-edit/recipe-edit.component.ts
+++ src/app/pages/recipe-edit/recipe-edit.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  FormArray,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
-} from '@angular/forms'; 
+} from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Recipe } from '../../model/recipe';
 import { RecipeService } from '../../services/recipe.service';
+import { CommonModule } from '@angular/common';
+import { Ingredient } from '../../model/ingredient';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.scss'],
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   standalone: true,
 })
 export class RecipeEditComponent implements OnInit {
@@ -23,7 +26,7 @@ export class RecipeEditComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private recipeService: RecipeService,
-    private formBuilder: FormBuilder 
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -44,12 +47,35 @@ export class RecipeEditComponent implements OnInit {
 
   // Az űrlap inicializálása
   private initForm(recipe?: Recipe) {
-    // recept betöltése vagy üres form
     this.recipeForm = this.formBuilder.group({
       name: [recipe ? recipe.name : '', Validators.required],
       imagePath: [recipe ? recipe.imagePath : '', Validators.required],
       description: [recipe ? recipe.description : '', Validators.required],
-      // A hozzávalókhoz hozzáadása 
+      // Hozzávalók
+      ingredients: this.formBuilder.array([]),
     });
+
+    // ha van hozzávalók betölti őket , amúgy üresset tölt be
+    if (recipe && recipe.ingredients) {
+      recipe.ingredients.map((ingredient: Ingredient) => {
+        this.addIngredient(ingredient.name, ingredient.amount);
+      });
+    } else {
+      this.addIngredient();
+    }
+  }
+
+  // input mező létrehozása 
+  addIngredient(name: string = '', amount: number = 0) {
+    const ingredientGroup = this.formBuilder.group({
+      name: [name, Validators.required],
+      amount: [amount, Validators.required],
+    });
+    //betöltés
+    this.ingredientsFormArray.push(ingredientGroup);
+  }
+  // html megjelenítés
+  get ingredientsFormArray() {
+    return this.recipeForm.get('ingredients') as FormArray;
   }
 }
