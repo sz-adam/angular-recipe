70c3876551d30b07d53eb9ff989629f6245fbc52
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-21 15:41:07 +0100

    routing new componens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,13 @@ import { Routes } from '@angular/router';
 import { RecipeComponent } from './pages/recipe/recipe.component';
 import { ShoppingComponent } from './pages/shopping/shopping.component';
 import { RecipeDetailComponent } from './pages/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './pages/recipe-edit/recipe-edit.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-
-  {
-    path: 'recipes',
-    component: RecipeComponent,
-   
-  }, 
-  { path: 'recipes/:id', component: RecipeDetailComponent },     
+  { path: 'recipes', component: RecipeComponent },
+  { path: 'recipes/:id', component: RecipeDetailComponent },
+  { path: 'new', component: RecipeEditComponent },
+  { path: ':id/edit', component: RecipeEditComponent },
   { path: 'shopping', component: ShoppingComponent },
 ];
