4b44575e58682fc954fcdd35e06acca8a0196dc6
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-22 20:15:00 +0100

    add ingredient function

--- src/app/components/add-shopping/add-shopping.component.ts
+++ src/app/components/add-shopping/add-shopping.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { ShoppingService } from '../../services/shopping.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-shopping',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-shopping.component.html',
   styleUrl: './add-shopping.component.scss'
 })
 export class AddShoppingComponent {
+  
+  constructor(private shoppingService: ShoppingService) {}
+
+  onSubmit(form: NgForm) {
+    const name = form.value.newIngredientName;
+    const amount = form.value.newIngredientAmount;    
+    this.shoppingService.addIngredients([{ name, amount }]);     
+    form.resetForm();
+  }
 
 }
