0886a03bba30d2ca325c1a8120759824e4c478b4
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-22 20:38:27 +0100

    function editmonde

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -2,16 +2,19 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../model/ingredient';
 import { ShoppingService } from '../../services/shopping.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule,FormsModule],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss',
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [];
+  editMode: boolean = false;
+  editingIndex: number | null = null;
 
   constructor(private shoppingService: ShoppingService) {}
   ngOnInit(): void {
@@ -22,5 +25,13 @@ export class ShoppingListComponent implements OnInit {
     console.log(this.ingredients)
   }
  
+  toggleEditMode(index: number) {
+    this.editMode = !this.editMode;
+    this.editingIndex = this.editMode ? index : null;
+  }
 
+  saveChanges() {
+    this.editMode = false;
+    this.editingIndex = null;
+  }
 }
