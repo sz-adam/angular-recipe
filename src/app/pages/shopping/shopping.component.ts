554f0dc507059bf6daa78881426802231b77bc65
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-22 09:24:16 +0100

    add new component shopping list

--- src/app/pages/shopping/shopping.component.ts
+++ src/app/pages/shopping/shopping.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ShoppingListComponent } from '../../components/shopping-list/shopping-list.component';
 
 @Component({
   selector: 'app-shopping',
   standalone: true,
-  imports: [],
+  imports: [ShoppingListComponent],
   templateUrl: './shopping.component.html',
   styleUrl: './shopping.component.scss'
 })
