e0e577d9986d673c51c7cbfa1f99ce6c5bc6c0cd
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-22 10:16:02 +0100

    new component

---
+++ src/app/components/add-shopping/add-shopping.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-shopping',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-shopping.component.html',
+  styleUrl: './add-shopping.component.scss'
+})
+export class AddShoppingComponent {
+
+}
