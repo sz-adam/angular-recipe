f91f32abeceaed081233d36c30c17b48ae8f3cd4
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-25 17:32:15 +0100

    update navbar logo navigate recipes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  constructor(private router:Router){}
+  ngOnInit(): void {
+  
+  }
+  recipesNavigate(){
+    this.router.navigate(['/recipes']);
+  }
 
 }
