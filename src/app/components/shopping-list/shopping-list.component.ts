715ef1bbda132e6d2ac2b7b314db70e43914ebdb
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-02-26 19:00:15 +0100

    shopping list edit disabled function

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../model/ingredient';
 import { ShoppingService } from '../../services/shopping.service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
@@ -15,28 +15,31 @@ export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [];
   editMode: boolean = false;
   editingIndex: number | null = null;
+  shoppingForm: FormGroup;
 
-  constructor(private shoppingService: ShoppingService) {}
+  constructor(private shoppingService: ShoppingService,private formBuilder: FormBuilder) {
+    this.shoppingForm = this.formBuilder.group({});
+  }
   ngOnInit(): void {
     this.ingredients = this.shoppingService.getIngredients();
-    this.shoppingService.ingredientsChanged.subscribe((ingredients: Ingredient[]) =>{
-      this.ingredients = ingredients
+    this.shoppingService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+      this.ingredients = ingredients;
     });
-    console.log(this.ingredients)
   }
- 
+
   toggleEditMode(index: number) {
     this.editMode = !this.editMode;
     this.editingIndex = this.editMode ? index : null;
   }
 
   saveChanges() {
-    this.editMode = false;
-    this.editingIndex = null;
-  }
-  deleteIngredient(editingIndex: number) {
-    this.shoppingService.deleteIngredient(editingIndex)
-
+    if (this.shoppingForm.valid) {
+      this.editMode = false;
+      this.editingIndex = null;
+    }
   }
 
+  deleteIngredient(index: number) {
+    this.shoppingService.deleteIngredient(index);
+  }
 }
